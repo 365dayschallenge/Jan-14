cd27b444cb122fdf7b5ad47dc351e7828719cbd0
Abby422 <abbyuri22@gmail.com>
2022-01-15 00:26:05 +0300

    first commit

---
+++ first.java
@@ -0,0 +1,19 @@
+class OOP {
+    public String name = "John";
+    protected Double fees;
+    protected Double fees_paid;
+    public Double fees_bal;
+
+    String Payfees(){
+        System.out.println("John");
+    }
+    private void register(){
+        System.out.println("Register");  
+    }
+
+    public static void main(String args[]) {
+        OOP obj = new OOP();
+        obj.Payfees();
+        obj.register();
+    }
+}
